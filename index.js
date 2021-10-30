7c2b84173144834ea28b0bc31887f5470db7973c
kaviramsv <kavitasvenkitachalam@gmail.com>
2021-10-30 14:42:50 +0000

    sum array added

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  // Sum all the numbers in the array[[1, 2, [[3], 4]], 5, []]
+  let sum=0;
+  array.forEach((item) => {
+    console.log("item",item,"************")
+    if (Array.isArray(item)) {
+      // Print out all it's items individually
+       
+       console.log("if",sum,item);
+       sum+=sumItems(item);
+    } else {
+        sum+=item;
+        //console.log(item);
+        console.log("else",sum,item);
+    }
+    
+  })
+  return sum;
 }
-
+//console.log(sumItems([1,2,3,4,5]));
+console.log(sumItems([[1, 2, [[3], 4]], 5, []]));
 module.exports = sumItems;
